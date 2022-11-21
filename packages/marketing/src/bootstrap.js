36a3c1dca71311d006828bc0067ac2b1c97b67f2
sfal <s.falchieri@gmail.com>
2022-11-21 17:40:28 +0100

    Add navigation between container and subapp

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // mount function to start the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history;
+
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 };
 
 // if in development and isolation, call mount immediately
@@ -12,7 +29,10 @@ if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {
+      // when in isolation, create browser history obj to make development easier
+      defaultHistory: createBrowserHistory(),
+    });
   }
 }
 
